a249609ff948f123d29fcb5548f8c8d6d1f9f1f7
marcWong <wangyao@ofo.so>
2016-08-22 17:40:11 -0700

    0822 implementation of 5 sets of data: 1.001A 2.002A 3.004B 4.028B 5.033A

--- classification.m
+++ classification.m
@@ -1,24 +1,33 @@
 %%
 %initialization
-path = '/Users/marcWong/Data/PerfusionSource/001A/PERFUSION/';
-%outputPath = '/Users/marcWong/Data/PerfusionSource/separation001A/';
-
+%path = '/Users/marcWong/Data/PerfusionSource/001A/PERFUSION/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/001A/';
 %path = '/Users/marcWong/Data/PerfusionSource/002A/ep2d_perf_p2/';
-%outputPath = '/Users/marcWong/Data/PerfusionSource/separation002A/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/002A/';
 %path = '/Users/marcWong/Data/PerfusionSource/003D/ep2d_Perf_Post/';
-%outputPath = '/Users/marcWong/Data/PerfusionSource/separation003D/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/003D/';
 %path = '/Users/marcWong/Data/PerfusionSource/004B/ep2d_Perfusion/';
-%outputPath = '/Users/marcWong/Data/PerfusionSource/separation004B/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/004B/';
 %path = '/Users/marcWong/Data/PerfusionSource/005C/ep2d_perf_p2/';
-%outputPath = '/Users/marcWong/Data/PerfusionSource/separation005C/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/005C/';
+%path = '/Users/marcWong/Data/PerfusionSource/028B/PERFUSION-THROM/';
+%outputPath = '/Users/marcWong/Data/ProcessedData/028B/';
+path = '/Users/marcWong/Data/PerfusionSource/033A/PERFUSION-DEDAS-ONO-THROM/';
+outputPath = '/Users/marcWong/Data/ProcessedData/033A/';
 listing = dir([path '*.dcm']);
 fileSum = length(listing); 
 info = dicominfo(strcat(path,listing(1).name));
 width = info.Width;
 height = info.Height;
-timePoint = 40;
-%layer may differ from sets of datas
-layer = 20;
+%%%%
+%patient   timePoint   layer      Sum
+%001A        40              20        800
+%002A        50              24        1200
+%004B        90              24        2160
+%028B        60              20        1200
+%033A        60              20       1200
+timePoint = 90;
+layer = 24;
 m = zeros([layer 1]);
 imgin = zeros([layer width height]);
 savedPic = uint16([width height]);
@@ -36,6 +45,7 @@ for imgNum=1:fileSum
     %tag calibration by its z coordinate
     switch info.ImagePositionPatient(3)
         %for 001A
+        %{
         case -33.671030050502
             tag=1;
         case -27.69087219833
@@ -76,8 +86,9 @@ for imgNum=1:fileSum
             tag=19;
         case 79.951982492222
             tag=20;
-        %{
+        %}
         %for 002A
+        %{
         case -45.805432408434
             tag=1;
         case -38.912625401598
@@ -126,9 +137,9 @@ for imgNum=1:fileSum
             tag=23;
         case 112.72918215455
             tag=24;
-        %}
-        %{
+            %}
         %for 003D
+        %{
         case -142.77616119385
             tag=1;
         case -136.77616119385
@@ -167,17 +178,158 @@ for imgNum=1:fileSum
             tag=18;
         case -22.776161193848
             tag=19;
+        case -16.7762
+            tag=20;
         case -10.776161193848
-3            tag=20;
-        case -4.7761611938477
             tag=21;
-        case 1.2238388061523
+        case -4.7761611938477
             tag=22;
+        case 1.2238388061523
+            tag=23;
         case 7.2238388061523
-            tag=233;
-        case 112.72918215455
             tag=24;
-        %} 
+        case 112.72918215455
+            tag=25;
+        case -124.7762
+            tag=26;
+            %}
+        %for004B
+        %{
+       case 7.4746298148064
+            tag=1;
+        case 12.972806389513
+            tag=2;
+        case 18.47098296422
+            tag=3;
+        case 23.969159538927
+            tag=4;
+        case 29.467336113635
+            tag=5;
+        case 34.965512688342
+            tag=6;
+        case 40.463689263049
+            tag=7;
+        case 45.961865837756
+            tag=8;
+        case 51.460040505114
+            tag=9;
+        case 56.958218033495
+            tag=10;
+        case 62.456393654528
+            tag=11;
+        case 67.954570467654
+            tag=12;
+        case 73.452746803942
+            tag=13;
+        case 78.950923378649
+            tag=14;
+        case 84.449099953356
+            tag=15;
+        case 89.947276528063
+            tag=16;
+        case 95.44545310277
+            tag=17;
+        case 100.94362777013
+            tag=18;
+        case 106.44180434484
+            tag=19;
+        case 111.93998091954
+            tag=20;
+        case 117.43815749425
+            tag=21;
+        case 122.93633406896
+            tag=22;
+        case 128.43451064366
+            tag=23;
+        case 133.93268721837
+            tag=24;
+        %}
+        %for 028B
+        %{
+        case -56.668674468994
+            tag=1;
+        case -49.668674468994
+            tag=2;
+        case -42.668674468994
+            tag=3;
+        case -35.668674468994
+            tag=4;
+        case -28.668674468994
+            tag=5;
+        case -21.668674468994
+            tag=6;
+        case -14.668674468994
+            tag=7;
+        case -7.6686744689941
+            tag=8;
+        case -0.6686747074127
+            tag=9;
+        case 6.3313255310059
+            tag=10;
+        case 13.331325531006
+            tag=11;
+        case 20.331325531006
+            tag=12;
+        case 27.331325531006
+            tag=13;
+        case 34.331325531006
+            tag=14;
+        case 41.331325531006
+            tag=15;
+        case 48.331325531006
+            tag=16;
+        case 55.331325531006
+            tag=17;
+        case 62.331325531006
+            tag=18;
+        case 69.331321716309
+            tag=19;
+        case 76.331321716309
+            tag=20;
+            %}
+        %033A
+        %{
+        case  -9.6662878
+            tag=1;
+        case -2.9721541
+            tag=2;
+        case 3.7219778
+            tag=3;
+        case 10.416112
+            tag=4;
+        case 17.110245
+            tag=5;
+        case 23.804378
+            tag=6;
+        case 30.498512
+            tag=7;
+        case 37.192645
+            tag=8;
+        case 43.886778
+            tag=9;
+        case 50.580911
+            tag=10;
+        case 57.275044
+            tag=11;
+        case 63.969178
+            tag=12;
+        case 70.66331
+            tag=13;
+        case 77.357443
+            tag=14;
+        case 84.051577
+            tag=15;
+        case 90.745711
+            tag=16;
+        case 97.439845
+            tag=17;
+        case 104.13397
+            tag=18;
+        case 110.82811
+            tag=19;
+        case 117.52224
+            tag=20;  
+        %}
         otherwise
             display(info.ImagePositionPatient(3));
     end
@@ -187,20 +339,9 @@ for imgNum=1:fileSum
             voxel(tag,m(tag),x,y) = imgin(x,y);
         end
     end
-    %{
-    filename = strcat(num2str(imgNum),'.jpg');
-    outputPathTmp = strcat(outputPath,num2str(tag));
-    outputPathTmp = strcat(outputPathTmp,'/');
-    imgin = (imgin - minG) ./ (maxG-minG);
-    imgin = uint8(imgin .* 255.);
-    imwrite(imgin,strcat(outputPathTmp,filename));
-    %}
-    %filename = strcat(num2str(imgNum),'.jpg');
-    %outputPathTmp = strcat(outputPath,num2str(tag));
-    %outputPathTmp = strcat(outputPathTmp,'/');
-    if(mod(imgNum,50)==0)
+    if(mod(imgNum,60)==0)
         display(strcat(num2str(imgNum),' processed'));
     end
 end
 
-dlmwrite(['/Users/marcWong/Data/ProcessedData/001A/voxel.txt'], voxel, 'delimiter', ' ');
+dlmwrite([outputPath 'voxel.txt'], voxel, 'delimiter', ' ');
--- concentration.m
+++ concentration.m
@@ -1,19 +1,37 @@
 %function C = concentrationComputation(layer)
 %%
 %constants
-TE = 54;
+%patient   timePoint   layer      Sum
+%001A        40              20        800
+%002A        50              24        1200
+%004B        90              24        2160
+%028B        60              20        1200
+%033A        60              20       1200
+TE = 32;
 k = 100;
-layer = 20;
-timePoint = 40;
-width = 256;
-height = 256;
-C = double([20 timePoint width height]);
-avgpath = '/Users/marcWong/Data/ProcessedData/001A/average/';
+layer = 24;
+timePoint = 50;
+width = 128;
+height = 128;
+ypsl = 1e-5;
+C = double([layer timePoint width height]);
+%path = '/Users/marcWong/Data/ProcessedData/001A/';
+path = '/Users/marcWong/Data/ProcessedData/002A/';
+%path = '/Users/marcWong/Data/ProcessedData/004B/';
+%path = '/Users/marcWong/Data/ProcessedData/028B/';
+%path = '/Users/marcWong/Data/ProcessedData/033A/';
+avgpath = [path 'average/'];
+
 %%
-voxel = load('/Users/marcWong/Data/ProcessedData/001A/voxel.txt');
+%voxel = load('/Users/marcWong/Data/ProcessedData/001A/voxel.txt');
+voxel = load('/Users/marcWong/Data/ProcessedData/002A/voxel.txt');
+%voxel = load('/Users/marcWong/Data/ProcessedData/004B/voxel.txt');
+%voxel = load('/Users/marcWong/Data/ProcessedData/028B/voxel.txt');
+%voxel = load('/Users/marcWong/Data/ProcessedData/033A/voxel.txt');
+
 voxel = reshape(voxel,layer,timePoint,width,height);
 voxel = double(voxel);
-for currentLayer = 1:20
+for currentLayer = 1:layer
     filename = strcat(num2str(currentLayer),'.txt');
     S0 = load(strcat(avgpath,filename));
     S0 = double(S0);
@@ -30,4 +48,4 @@ for currentLayer = 1:20
     end
     display([num2str(currentLayer) ' processed']);
 end 
-dlmwrite('/Users/marcWong/Data/ProcessedData/001A/concentration.txt', C, 'delimiter', ' ','precision',10);
+dlmwrite([path 'concentration.txt'], C, 'delimiter', ' ','precision',10);
